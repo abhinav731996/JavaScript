330085c7260f9cea62632b2683b0d2201f27524b
Abhinav <abhinav731996@gmail.com>
2025-12-16 11:21:44 +0530

    ytube & class

---
+++ class/loops&conditions/script.js
@@ -0,0 +1,23 @@
+let score = 78;
+
+switch (true) {
+    case score >= 80:
+        console.log("A");
+        break;
+
+    case score >= 60:
+        console.log("B");
+        break;
+
+    case score >= 40:
+        console.log("C");
+        break;
+
+    case score >= 30:
+        console.log("D");
+        break;
+
+    default:
+        console.log("Fail");
+        break;
+}
---
+++ ytube/lect-3/script.js
@@ -0,0 +1,12 @@
+// loops
+
+for (let index = 0; index < 5; index++) {
+    console.log("hello world");
+}
+
+let sum = 0;
+for (let i = 1; i <=5; i++) {
+    sum = sum + i;
+}
+
+console.log("sum = ", sum);
