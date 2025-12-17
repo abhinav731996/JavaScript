8b80e6c2aa82a1b33898b2dc80b8155f259781a7
Abhinav <abhinav731996@gmail.com>
2025-12-17 12:14:34 +0530

    loops

--- class/loops-conditions/script.js
+++ class/loops-conditions/script.js
@@ -21,3 +21,28 @@ switch (true) {
         console.log("Fail");
         break;
 }
+
+
+// for loop
+for (let i = 1; i <=10; i++) {
+    console.log(`2 * ${i} = ${2*i}`);
+}
+
+// do while loop
+let i = 1;
+do {
+    console.log(`3 * ${i} = ${3*i}`);
+    i++;
+} while (i<=10);
+
+// while
+while (i<=10) {
+    console.log(`4 * ${i} = ${4*i}`);
+    i++;
+};
+
+let str = "JavaScript";
+for (const i of str) {
+    console.log("i = ", i);
+    
+}
--- ytube/lect-3/script.js
+++ ytube/lect-3/script.js
@@ -1,12 +1,65 @@
-// loops
+// // loops
 
-for (let index = 0; index < 5; index++) {
-    console.log("hello world");
-}
+// for (let index = 0; index < 5; index++) {
+//     console.log("hello world");
+// }
 
-let sum = 0;
-for (let i = 1; i <=5; i++) {
-    sum = sum + i;
-}
+// let sum = 0;
+// for (let i = 1; i <=5; i++) {
+//     sum = sum + i;
+// }
 
-console.log("sum = ", sum);
+// console.log("sum = ", sum);
+
+// // do while loop
+// let i = 1;
+// do {
+//     console.log(`3 * ${i} = ${3*i}`);
+//     i++;
+// } while (i<=10);
+
+// // while
+// while (i<=10) {
+//     console.log(`4 * ${i} = ${4*i}`);
+//     i++;
+// };
+
+
+// for of loop 
+
+// let str = "JavaScript";
+
+// let size = 0;
+
+// for (const i of str) {
+//     console.log("i = ", i);
+//     size++;
+// }
+// console.log("string size =", size);
+
+
+
+// for in loop 
+
+// let student ={
+//     name : "Rahul",
+//     age: 20,
+//     cgpa: 7.5,
+//     isPass: true,
+// }
+
+// for(let key in student){
+//     // console.log(key);
+//     console.log("key =", key, "value = ", student[key]);
+    
+// }
+
+
+// Questions
+
+// find even number from 0-100
+
+let word ="hello";
+console.log(word.length);
+
+
