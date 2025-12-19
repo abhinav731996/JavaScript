2588044a26c66eab5ed3208b5efc9aaa3d10e526
Abhinav <abhinav731996@gmail.com>
2025-12-19 11:09:34 +0530

    functions

---
+++ class/function/script.js
@@ -0,0 +1,57 @@
+// // function myFunction(p1, p2) {
+// //   return p1 * p2;
+// // }
+
+// // let result = myFunction(4, 3);
+// // console.log(result)
+
+
+
+// function greet(name, msg){
+//     console.log("hello " + name + "! " + msg);
+// }
+// greet("Abhi", "gud mrng");
+
+
+
+// function add(a,b){
+//     let sum = +a + +b;
+//     // console.log(sum);    
+//     return sum;
+// }
+// let sum = add(4,5);
+// console.log(sum);
+
+
+// function addTwoNumb(num1, num2){
+//     let result = num1 + num2;
+//     return result
+// }
+
+// const result = addTwoNumb(3,4);
+// console.log("Result = ", result);
+
+
+// function userLogIn(username){
+//     return `${username} just logged in`
+// }
+// console.log(userLogIn("Abhinav"));
+
+// function userLogIn(username){
+//     if(username === undefined){
+//         console.log("please enter username");
+//         return
+//     }
+//     return `${username} just logged in`
+// }
+// console.log(userLogIn());
+
+
+
+let num = [1,2,3,4,5,6];
+let result =[];
+for (let i = 0; i < num.length; i++) {
+    result.push(num[i] + 2);
+    
+}
+console.log(result);
