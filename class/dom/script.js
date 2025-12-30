00fbf25a4bb2c59bc8b6d22d11c0067e247dc564
Abhinav <abhinav731996@gmail.com>
2025-12-30 10:52:08 +0530

    task-3

---
+++ class/dom/script.js
@@ -0,0 +1,9 @@
+
+console.log(document.title);
+console.log(document.URL);
+console.log(document.head);
+console.log(document.body);
+
+
+let mainHeading = document.getElementById("main-heading");
+console.log(mainHeading);
