a3aedc926aeb245669efc28ec8b8c00ba58acc3e
Abhinav <abhinav731996@gmail.com>
2026-05-07 13:41:38 +0530

    functions practice

--- ytube/practice/script.js
+++ ytube/practice/script.js
@@ -1,1 +1,13 @@
-console.log("Abhinav");
+
+// ************* Reduce function **************
+// // console.log("Abhinav");
+
+// const arr = [[1,2], [3,4]];
+
+// const flat = arr.reduce((acc, curr) => acc.concat(curr), []);
+
+// console.log(flat);
+// console.log(arr);
+
+
+// ************* Map function **************
