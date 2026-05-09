326b40f7366735d4a9a524341dd52089365f9383
Abhinav <abhinav731996@gmail.com>
2026-05-09 10:13:11 +0530

    interview practice

--- ytube/practice/script.js
+++ ytube/practice/script.js
@@ -2,12 +2,49 @@
 // // ************* Reduce function **************
 // console.log("Abhinav");
 
-const arr = [[1,2], [3,4]];
+// const arr = [[1,2], [3,4]];
 
-const flat = arr.reduce((acc, curr) => acc.concat(curr), []);
+// const flat = arr.reduce((acc, curr) => acc.concat(curr), []);
 
-console.log(flat);
-console.log(arr);
+// console.log(flat);
+// console.log(arr);
 
 
-// ************* Map function **************
+// ************* practice **************
+
+// console.log(0.2 + 0.3 === 0.5);
+// console.log(0.2 + 0.3 == 0.5);
+// console.log(0.2 + 0.3);
+// ------------------------------------------
+
+// const obj = {
+//     a:1,
+//     b:2
+// }
+// let newObj = obj.toString();
+// console.log(newObj);
+// -------------------// to find max product of an Array-----------------------
+
+// 
+// const arr = [1, 2, 3, 4, 5, 6];
+
+// let maxProd = 0;
+
+// for (let i = 0; i < arr.length; i++) {
+//     for (let j = i+1; j < arr.length; j++) {
+//         const product = arr[i] * arr[j];
+
+//         if (product > maxProd) {
+//             maxProd = product;
+//         }
+        
+//     }
+// }
+
+// console.log("MAX PRODUCT: ", maxProd);
+
+// ---------------------// for shorting array -----------------------
+
+// let num = [1, 4,7, 3, 6, 9, 8];
+// console.log(num.sort((a,b) => a-b));
+
