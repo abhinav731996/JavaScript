b541e48a152201d53cd87be641090319d8db9300
Abhinav <abhinav731996@gmail.com>
2026-05-10 16:56:46 +0530

    DOM practice

--- ytube/practice/script.js
+++ ytube/practice/script.js
@@ -48,3 +48,71 @@
 // let num = [1, 4,7, 3, 6, 9, 8];
 // console.log(num.sort((a,b) => a-b));
 
+
+// ---------------------- classes -----------------------------
+
+// class user {
+//     constructor(name, phone, username, password) {
+//         this.name = name;
+//         this.phone = phone;
+//         this.username = username;
+//         this.password = password;
+//     }
+//     updatePassword(pass){
+//         return this.password = pass;
+//     }
+//     getUserName() {
+//         return this.username;
+//     } 
+// }
+
+// let abhi = new user("Abhinav", "9034340569", "abhi90@gmail.com", "@abhi12")
+
+// console.log(abhi);
+
+// console.log(abhi.getUserName());
+// console.log(abhi.updatePassword("Abhi@12"));
+
+// console.log(abhi);
+
+
+// ----------------------------- DOM ----------------------------
+
+// console.log(document.title);
+// console.log(document.body);
+// console.log(document.URL);
+
+
+// // #### adding & removing ####
+// function addBtn() {
+//     const box = document.createElement("div");
+//     box.innerHTML = "New Box";
+//     box.id = "box";
+//     document.getElementById("container").appendChild(box);
+// }
+
+// function removeBtn() {
+//     const box = document.getElementById("box");
+//     if (box) {
+//         box.remove();
+//     }
+// }
+
+// // #### DOM Event ####
+
+//****** */ add event listener
+document.getElementById("btn").addEventListener("click", function () {
+    alert("Button Clicked!!!")
+})
+
+// ******** remove event listener
+
+const addBtn = document.getElementById("addBtn");
+const removeBtn = document.getElementById("removeBtn");
+
+function sayHello() {
+    alert("Hello Codder !!")
+}
+
+addBtn.addEventListener("click", sayHello);
+removeBtn.addEventListener("click", )
